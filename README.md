fca1333210d5f5130a97181e808cfe684d7ab9cb
Igor0Pires <igorpife@usp.br>
2025-04-12 22:07:30 -0300

    Feat: Adiciona script para salvar cookies manualmente usando Playwright

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## get_cookies.py:
+### Bibliotecas: 
+**Funções Assíncronas**:
+- `playwright.async_api`: Biblioteca para automação de navegadores.
+- `asyncio`: Biblioteca para trabalhar com programação assíncrona em Python.
+
+**Manipulação de Arquivos**:
+- `pickle`: Biblioteca para salvar e carregar objetos Python em arquivos
+
+### Definições Importantes: 
+**Funções Assíncronas**: Funções que permitem a execução de tarefas de forma não bloqueante, possibilitando que outras operações sejam realizadas enquanto uma tarefa aguarda sua conclusão.
+
+**Browser Context**: Um ambiente isolado dentro de um navegador que permite executar ações sem interferir em outros contextos.
+
+**Cookies**: Pequenos arquivos armazenados no navegador que contêm informações sobre sessões, preferências do usuário e outros dados relevantes para a navegação.
+
+
---
+++ get_cookies.py
@@ -0,0 +1,31 @@
+from playwright.async_api import async_playwright # biblioteca para automação de navegadores
+import pickle # biblioteca para salvar e carregar objetos Python em arquivos
+import asyncio # biblioteca para trabalhar com funções assíncronas (playwright é assíncrono)
+
+url = "https://app.datacamp.com"
+
+
+# Fazer login manualmente no navegador e salvar os cookies
+async def save_cookies_manually(): # definição da função assíncrona
+    async with async_playwright() as p: # inicia o Playwright
+
+        browser = await p.chromium.launch(headless=False) # inicia o navegador Chromium (pode ser Firefox ou WebKit também)
+        # headless=True para rodar em segundo plano (sem interface gráfica), False para ver o navegador
+        
+        context = await browser.new_context() # cria um novo browser context (sem cookies ou cache)
+        page = await context.new_page() # cria uma nova página no navegador
+
+        await page.goto(url, timeout=60000) # navega para a página de login do DataCamp
+
+        await context.clear_cookies()  # Antes de adicionar os novos
+
+        # Faça o login aqui
+        input("⚠️ Faça login. Depois pressione Enter...")
+        # depois de fazer login e carregar a página inicial, pressione Enter no terminal para continuar
+
+        # Salva os cookies
+        cookies = await context.cookies() # await serve para esperar a execução da função assíncrona terminar antes de continuar
+        with open("cookies.pkl", "wb") as f:
+            pickle.dump(cookies, f)
+
+asyncio.run(save_cookies_manually())
